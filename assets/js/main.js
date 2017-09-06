dd7136de378eedd255c1302da260c9a95a3f3e66
Eliez MAIGNÉ <eliez_lp@hotmail.com>
2017-09-06 18:52:33 +0200

    First commit
    Html and CSS basic settings
    Js basic settings 2 questions for test
    main.js barely started

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var question = document.getElementById('question');
+var ch1 = document.getElementById('choice1');
+var ch2 = document.getElementById('choice2');
+var ch3 = document.getElementById('choice3');
+var ch4 = document.getElementById('choice4');
+
+var nextBtn = document.getElementById('next');
+var prevBtn = document.getElementById('previous');
---
+++ assets/js/questions.js
@@ -0,0 +1,16 @@
+var questions = [{
+    "question": "Elle est où la poulette ?",
+    "1": "Elle est bien cachée ?!",
+    "2":"Pas du tout les lapins, les lapins c'est gentils",
+    "3":"Obi-Wan Kenobi",
+    "4":"dans le poulailler voyons !",
+    "correctAnswer": "1"
+    }
+    {
+    "question": "Une facile: 2x2 ?",
+    "1": "42",
+    "2":"Je prends la main.",
+    "3":"Obi-Wan Kenobi",
+    "4":"D la réponse D.",
+    "correctAnswer": "1"
+    }];
