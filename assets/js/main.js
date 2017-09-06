a933afe2602823b331e10376be8c302929167025
Eliez MAIGNÉ <eliez_lp@hotmail.com>
2017-09-06 20:28:53 +0200

    Question display
    question.js deleted couldn't find a way to make it communicate with my main css.
    question display ready 
    changes in Html/CSS: no more row or column just spans inputs and label.
    Found bootstrap buttons for the different answer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,25 @@
-var question = document.getElementById('question');
+var currentQuestion = 0;
+//var total = question.length;
+
+var questions = [{
+    "question": "Elle est où la poulette ?",
+    "answer1": "Elle est bien cachée ?!",
+    "answer2":"Pas du tout les lapins, les lapins c'est gentils",
+    "answer3":"Obi-Wan Kenobi",
+    "answer4":"dans le poulailler voyons !",
+    "correctAnswer": "1"
+    },
+    {
+    "question": "Une facile: 2x2 ?",
+    "answer1": "42",
+    "answer2":"Je prends la main.",
+    "answer3":"Obi-Wan Kenobi",
+    "answer4":"D la réponse D.",
+    "correctAnswer": "2"
+    }];
+
+var ask = document.getElementById('ask');
+
 var ch1 = document.getElementById('choice1');
 var ch2 = document.getElementById('choice2');
 var ch3 = document.getElementById('choice3');
@@ -6,3 +27,13 @@ var ch4 = document.getElementById('choice4');
 
 var nextBtn = document.getElementById('next');
 var prevBtn = document.getElementById('previous');
+
+function load (questionIndex){
+    var i = questions[questionIndex];
+    ask.textContent = (questionIndex +1)+'. '+i.question;
+    ch1.textContent = i.answer1;
+    ch2.textContent = i.answer2;
+    ch3.textContent = i.answer3;
+    ch4.textContent = i.answer4;
+}
+load(currentQuestion);
